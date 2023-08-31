ebcd8d0d131834cecc69b938ff1c5d105baf5ad6
kierav <kiera.vds@gmail.com>
2023-08-31 14:55:02 -0600

    reused dataloader code from other projects

---
+++ src/data.py
@@ -0,0 +1,136 @@
+from datetime import datetime,timedelta
+import glob
+import torch
+import torchvision.transforms.v2 as transforms
+import h5py
+import numpy as np
+import pandas as pd
+import pytorch_lightning as pl
+from torch.utils.data import Dataset,DataLoader
+from utils import split_data
+
+class SharpsDataset(Dataset):
+    """
+        Pytorch dataset for handling magnetogram tile data 
+        
+    """
+    def __init__(self, image_files: list, transform, augmentation: str='single',
+                normalize=False):
+        '''
+            Initializes image files in the dataset
+            
+            Args:
+                image_files (list): list of file paths to images
+                transform:          single or list of torchvision transforms
+        '''
+        self.image_files = image_files
+        self.transform = transform
+
+    def __len__(self):
+        '''
+            Calculates the number of images in the dataset
+                
+            Returns:
+                int: number of images in the dataset
+        '''
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        '''
+            Retrieves an image from the dataset and creates a copy of it,
+            applying a series of random augmentations to the copy.
+
+            Args:
+                idx (int): index of the image to retrieve
+                
+            Returns:
+                tuple: (image, image2) where image2 is an augmented modification
+                of the input and image can be the original image, or another augmented
+                modification, in which case image2 is double augmented
+        '''
+        file = self.image_files[idx]
+        image = np.array(h5py.File(file,'r')['hmi']).astype(np.float32)
+        image = np.nan_to_num(image)
+
+        # Normalize magnetogram data
+        # clip magnetogram data within max value
+        maxval = 1000  # Gauss
+        image[np.where(image>maxval)] = maxval
+        image[np.where(image<-maxval)] = -maxval
+        # scale between 0 and 1
+        image = (image+maxval)/2/maxval
+        image = np.expand_dims(image,0)
+
+        image = self.transform(image)    
+
+        return file,image
+
+    
+class SharpsDataModule(pl.LightningDataModule):
+    """
+    Datamodule for self supervision on tiles dataset
+    """
+
+    def __init__(self,data_file:str,batch:int=128,
+                dim:int=128,val_split:int=0,test:str=''):
+        super().__init__()
+        self.data_file = data_file
+        self.batch_size = batch
+        self.val_split = val_split
+        self.test = test
+
+        # define data transforms - augmentation for training
+        self.training_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485,),std=(0.229,)),
+            transforms.RandomInvert(p=0.3),
+            transforms.RandomAdjustSharpness(1.5,p=0.3),
+            transforms.RandomApply(torch.nn.ModuleList([
+                 transforms.GaussianBlur(kernel_size=9,sigma=(0.1,4.0)),
+                 ]),p=0.3),
+            transforms.RandomApply(torch.nn.ModuleList([
+                 transforms.RandomRotation(degrees=45,fill=0.5)
+                 ]),p=0.3),
+            transforms.RandomVerticalFlip(p=0.3),
+            transforms.ScaleJitter(target_size=(128,128),scale_range=(0.7,1.3),antialias=True),
+            transforms.Resize((dim,dim),antialias=True),
+        ])
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485,),std=(0.229,)),
+            transforms.Resize((dim,dim),antialias=True),
+        ])
+
+    def prepare_data(self):
+        self.df = pd.read_csv(self.data_file)
+        self.df['sample_time'] = pd.to_datetime(self.df['sample_time'],format='mixed')
+
+        
+    def setup(self,stage:str):
+        # split into training and validation
+        df_test,df_pseudotest,self.df_train,df_val = split_data(self.df,self.val_split,self.test)
+        self.train_set = SharpsDataset(self.df_train['filename'].tolist(),self.training_transform)
+        self.val_set = SharpsDataset(df_val['filename'].tolist(),self.transform)
+        self.pseudotest_set = SharpsDataset(df_pseudotest['filename'].tolist(),self.transform)
+        self.test_set = SharpsDataset(df_test['filename'].tolist(),self.transform)
+        self.trainval_set = SharpsDataset(pd.concat([self.df_train,df_val])['filename'].tolist(),self.transform)
+
+    def subsample_trainset(self,filenames):
+        # given a list of filenames, subsample so the train set only includes files from that list
+        subset_filenames = self.df_train['filename'][self.df_train['filename'].isin(filenames)].tolist()
+        self.subset_train_set = SharpsDataset(subset_filenames,self.training_transform)
+
+    def subset_train_dataloader(self,shuffle=True):
+        return DataLoader(self.subset_train_set,batch_size=self.batch_size,num_workers=4,shuffle=shuffle)
+    
+    def train_dataloader(self,shuffle=True):
+        return DataLoader(self.train_set,batch_size=self.batch_size,num_workers=4,shuffle=shuffle)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_set,batch_size=self.batch_size,num_workers=4)
+    
+    def pseudotest_dataloader(self):
+        return DataLoader(self.pseudotest_set,batch_size=self.batch_size,num_workers=4)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_set,batch_size=self.batch_size,num_workers=4)
