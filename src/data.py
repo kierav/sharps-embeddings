a0078b3278e34a3d00d135e46ab685e187a0e817
kierav <kiera.vds@gmail.com>
2023-09-01 10:06:15 -0600

    change input to dataset to pandas dataframe instead of list of files

--- src/data.py
+++ src/data.py
@@ -14,16 +14,19 @@ class SharpsDataset(Dataset):
         Pytorch dataset for handling magnetogram tile data 
         
     """
-    def __init__(self, image_files: list, transform, augmentation: str='single',
-                normalize=False):
+    def __init__(self, df: pd.DataFrame, transform, feature_cols:list=None):
         '''
             Initializes image files in the dataset
             
             Args:
-                image_files (list): list of file paths to images
+                df (dataframe):     Pandas dataframe containing filenames and labels 
                 transform:          single or list of torchvision transforms
+                feature_cols (list): names of scalar feature columns
         '''
-        self.image_files = image_files
+        self.name_frame = df.loc[:,'filename']
+        if feature_cols == None:
+            feature_cols = []
+        self.features = df.loc[:,feature_cols]
         self.transform = transform
 
     def __len__(self):
@@ -33,7 +36,7 @@ class SharpsDataset(Dataset):
             Returns:
                 int: number of images in the dataset
         '''
-        return len(self.image_files)
+        return len(self.name_frame)
 
     def __getitem__(self, idx):
         '''
@@ -48,7 +51,7 @@ class SharpsDataset(Dataset):
                 of the input and image can be the original image, or another augmented
                 modification, in which case image2 is double augmented
         '''
-        file = self.image_files[idx]
+        file = self.name_frame.iloc[idx]
         image = np.array(h5py.File(file,'r')['hmi']).astype(np.float32)
         image = np.nan_to_num(image)
 
@@ -63,7 +66,9 @@ class SharpsDataset(Dataset):
 
         image = self.transform(image)    
 
-        return file,image
+        features = torch.Tensor(self.features.iloc[idx])
+
+        return file,image,features
 
     
 class SharpsDataModule(pl.LightningDataModule):
@@ -72,12 +77,15 @@ class SharpsDataModule(pl.LightningDataModule):
     """
 
     def __init__(self,data_file:str,batch:int=128,
-                dim:int=128,val_split:int=0,test:str=''):
+                dim:int=128,val_split:int=0,test:str='',features:list=None):
         super().__init__()
         self.data_file = data_file
         self.batch_size = batch
         self.val_split = val_split
         self.test = test
+        if features == None:
+            features = []
+        self.features = features
 
         # define data transforms - augmentation for training
         self.training_transform = transforms.Compose([
@@ -109,16 +117,16 @@ class SharpsDataModule(pl.LightningDataModule):
     def setup(self,stage:str):
         # split into training and validation
         df_test,df_pseudotest,self.df_train,df_val = split_data(self.df,self.val_split,self.test)
-        self.train_set = SharpsDataset(self.df_train['filename'].tolist(),self.training_transform)
-        self.val_set = SharpsDataset(df_val['filename'].tolist(),self.transform)
-        self.pseudotest_set = SharpsDataset(df_pseudotest['filename'].tolist(),self.transform)
-        self.test_set = SharpsDataset(df_test['filename'].tolist(),self.transform)
-        self.trainval_set = SharpsDataset(pd.concat([self.df_train,df_val])['filename'].tolist(),self.transform)
+        self.train_set = SharpsDataset(self.df_train,self.training_transform,self.features)
+        self.val_set = SharpsDataset(df_val,self.transform,self.features)
+        self.pseudotest_set = SharpsDataset(df_pseudotest,self.transform)
+        self.test_set = SharpsDataset(df_test,self.transform,self.features)
+        self.trainval_set = SharpsDataset(pd.concat([self.df_train,df_val]),self.transform,self.features)
 
     def subsample_trainset(self,filenames):
         # given a list of filenames, subsample so the train set only includes files from that list
-        subset_filenames = self.df_train['filename'][self.df_train['filename'].isin(filenames)].tolist()
-        self.subset_train_set = SharpsDataset(subset_filenames,self.training_transform)
+        subset_df = self.df_train[self.df_train['filename'].isin(filenames)]
+        self.subset_train_set = SharpsDataset(subset_df,self.training_transform,self.features)
 
     def subset_train_dataloader(self,shuffle=True):
         return DataLoader(self.subset_train_set,batch_size=self.batch_size,num_workers=4,shuffle=shuffle)
