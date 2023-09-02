5d41b53d91ca5c9f3353511f6b315bc804d16da9
subhamoysgit <sc8473@gmail.com>
2023-09-01 21:18:50 -0600

    added autoencoder model architecture

---
+++ src/autoencoder.py
@@ -0,0 +1,163 @@
+import matplotlib.pyplot as plt
+import pytorch_lightning as pl
+import seaborn as sns
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+from IPython.display import set_matplotlib_formats
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from tqdm.notebook import tqdm
+from dataset import SHARPdataset
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.determinstic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
+print("Device:", device)
+class Encoder(nn.Module):
+    def __init__(self, num_input_channels: int, base_channel_size: int,
+                 latent_dim: int, act_fn: object = nn.ReLU,
+                 image_size: int = 256):
+        """
+        Args:
+           num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
+           base_channel_size : Number of channels we use in the first convolutional layers. 
+                                Deeper layers might use a duplicate of it.
+           latent_dim : Dimensionality of latent representation z
+           act_fn : Activation function used throughout the encoder network
+        """
+        super().__init__()
+        c_hid = base_channel_size
+        self.net = nn.Sequential(
+            nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2),  # 128x128 => 64x64
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(c_hid, 2 * c_hid, kernel_size=3, padding=1, stride=2),  # 64x64 => 32x32
+            act_fn(),
+            nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1, stride=2),  # 32x32 => 16x16
+            act_fn(),
+            nn.Flatten(),  # Image grid to single feature vector
+            nn.Linear(2 * ((image_size//8)**2) * c_hid, latent_dim),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class Decoder(nn.Module):
+    def __init__(self, num_input_channels: int, base_channel_size: int,
+                 latent_dim: int, act_fn: object = nn.ReLU,
+                 image_size: int = 256):
+        """
+        Args:
+           num_input_channels : Number of channels of the image to reconstruct. For CIFAR, this parameter is 3
+           base_channel_size : Number of channels we use in the last convolutional layers.
+                                Early layers might use a duplicate of it.
+           latent_dim : Dimensionality of latent representation z
+           act_fn : Activation function used throughout the decoder network
+        """
+        super().__init__()
+        c_hid = base_channel_size
+        self.image_size = image_size
+        self.linear = nn.Sequential(nn.Linear(latent_dim, 2 * ((image_size//8)**2) * c_hid), act_fn())
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(
+                2 * c_hid, 2 * c_hid, kernel_size=3, output_padding=1, padding=1, stride=2
+            ),  # 4x4 => 8x8
+            act_fn(),
+            nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(2 * c_hid, c_hid, kernel_size=3, output_padding=1, padding=1, stride=2),  # 8x8 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(
+                c_hid, num_input_channels, kernel_size=3, output_padding=1, padding=1, stride=2
+            ),  # 16x16 => 32x32
+            nn.Sigmoid(),  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.reshape(x.shape[0], -1, (self.image_size//8), (self.image_size//8))
+        x = self.net(x)
+        return x
+
+
+class SharpEmbedder(pl.LightningModule):
+    def __init__(
+        self,
+        base_channel_size: int,
+        latent_dim: int,
+        encoder_class: object = Encoder,
+        decoder_class: object = Decoder,
+        num_input_channels: int = 3,
+        image_size: int = 256
+    ):
+        super().__init__()
+        # Saving hyperparameters of autoencoder
+        self.save_hyperparameters()
+        # Creating encoder and decoder
+        self.encoder = encoder_class(num_input_channels, base_channel_size, latent_dim, image_size=image_size)
+        self.decoder = decoder_class(num_input_channels, base_channel_size, latent_dim, image_size=image_size)
+        # Example input array needed for visualizing the graph of the network
+
+    def forward(self, x):
+        """The forward function takes in an image and returns the reconstructed image."""
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+
+    def _get_reconstruction_loss(self, batch):
+        """Given a batch of images, this function returns the reconstruction loss (MSE in our case)"""
+        x, _ = batch  # We do not need the labels
+        x_hat = self.forward(x)
+        loss = F.mse_loss(x, x_hat, reduction="none")
+        loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        # Using a scheduler is optional but can be helpful.
+        # The scheduler reduces the LR if the validation performance hasn't improved for the last N epochs
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.2, patience=20, min_lr=5e-5)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+
+    def training_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log("val_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log("test_loss", loss)
+
+if __name__=='__main__':
+    data_path = '/d0/kvandersande/sharps_hdf5/'
+    image_size = 256
+    dataset = SHARPdataset(data_path,image_size=image_size)
+    (image, filename) = dataset[1]
+    model = SharpEmbedder(16, 20, encoder_class = Encoder,
+                          decoder_class = Decoder,
+                          num_input_channels = 4,
+                          image_size=image_size)
+    image_out = model(image)
+    print(image.shape)
+    print(image_out.shape)
+    #print(model)
