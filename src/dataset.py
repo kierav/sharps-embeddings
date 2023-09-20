e97dd24eb0a79aa5d75c37fb56d6d6067964bf51
subhamoysgit <sc8473@gmail.com>
2023-09-20 13:39:35 -0600

    added crop functionality

--- src/dataset.py
+++ src/dataset.py
@@ -4,11 +4,14 @@ import numpy as np
 import h5py
 import torch, torchvision
 from PIL import Image
+import pandas as pd
 
 class SHARPdataset(Dataset):
 
     def __init__(self, data_path: str, param: bin=False,
-                 data_stride:int = 1, image_size = 256, datatype=np.float32):
+                 data_stride:int = 1, image_size = 256,
+                 datatype=np.float32,
+                 crop:bool = False):
         '''
             Initializes image files in the dataset
             
@@ -20,12 +23,20 @@ class SHARPdataset(Dataset):
                 param (bin): add SHARP parameters
         '''
         self.data_path = data_path
-        self.sharp_images = glob.glob(data_path + "/*.h5")
+        self.image_size = image_size
+        self.crop = crop
+        if self.crop:
+            df = pd.read_csv('/d0/kvandersande/index_sharps.csv')
+            df_ = df[df['naxis1']>self.image_size][df['naxis2']>self.image_size]
+            self.sharp_images = list(df_['file'])
+        else:
+            self.sharp_images = glob.glob(data_path + "/*.h5")
         if data_stride>1:
             self.sharp_images = self.sharp_images[::data_stride]
-        self.image_size = image_size
+        
         self.param = param
         self.datatype = datatype
+        
         if self.param is True:
             'read the csv of SHARP params'
 
@@ -56,8 +67,16 @@ class SHARPdataset(Dataset):
         data = np.nan_to_num(data, copy=False, nan=0.5, posinf=1, neginf=0)
         data = data[None, :, :, :]
         data = torch.from_numpy(data.astype(self.datatype))
-        resize = torchvision.transforms.Resize((self.image_size, self.image_size))
-        data = resize(data)
+        if self.crop:
+            cen_row = data.shape[2]//2
+            cen_col = data.shape[3]//2
+            half_size = self.image_size//2
+            data = data[:, :,
+                        (cen_row - half_size):(cen_row + half_size),
+                        (cen_col - half_size):(cen_col + half_size)]
+        else:
+            resize = torchvision.transforms.Resize((self.image_size, self.image_size))
+            data = resize(data)
         
         if self.param is True:
             return ('SHARP_PARAMS [idx]', data)
